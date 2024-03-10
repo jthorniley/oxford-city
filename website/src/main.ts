237f18622452375add497ea9f858381c465ec9a2
James Thorniley <james.thorniley@gmail.com>
2024-03-10 18:02:11 +0000

    add some results and a callback

--- website/src/main.ts
+++ website/src/main.ts
@@ -1,8 +1,21 @@
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import oxfordWards from "./oxford-wards.json";
+import results2022 from "./results-2022.json";
 import "./styles.css";
 
+function activateWard(wardName: string) {
+  const wardResults = results2022[wardName] as any;
+  if (wardResults !== undefined) {
+    console.log(wardResults);
+    const container = document.getElementById("info-container");
+    if (container) {
+      container.innerHTML = `
+      <h3>${wardName}</h3>
+      `
+    }
+  }
+}
 
 function createMap() {
   var map = L.map('map').setView([51.755409, -1.255782], 12);
@@ -12,18 +25,24 @@ function createMap() {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map);
 
-  L.geoJSON(oxfordWards,
+  L.geoJSON(oxfordWards as any,
     {
-      style: (feature) => {
-        console.log(feature)
+      style: () => {
         return {
           stroke: true,
-          weight: 2,
+          weight: 1,
           color: "red",
           opacity: 1,
           fill: true
         };
-      }
+      },
+      onEachFeature(feature, layer) {
+        const wardName = feature.properties.Ward_name;
+
+        layer.bindTooltip(wardName);
+
+        layer.addEventListener("mouseover", () => activateWard(wardName));
+      },
     }
   ).addTo(map);
 }
