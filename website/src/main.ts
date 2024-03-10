394c26a5324d76bdb9ad4e8fa578acb2c643df31
James Thorniley <james.thorniley@gmail.com>
2024-03-10 08:50:54 +0000

    show wards on map

--- website/src/main.ts
+++ website/src/main.ts
@@ -1,14 +1,31 @@
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import oxfordWards from "./oxford-wards.json";
 import "./styles.css";
 
+
 function createMap() {
-  var map = L.map('map').setView([51.755409, -1.255782], 13);
+  var map = L.map('map').setView([51.755409, -1.255782], 12);
 
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map);
+
+  L.geoJSON(oxfordWards,
+    {
+      style: (feature) => {
+        console.log(feature)
+        return {
+          stroke: true,
+          weight: 2,
+          color: "red",
+          opacity: 1,
+          fill: true
+        };
+      }
+    }
+  ).addTo(map);
 }
 
 createMap();
