06497bc4b217bd8492f4a90aecdfe1b5cc8b6ff5
James Thorniley <james.thorniley@gmail.com>
2024-03-16 09:22:10 +0000

    create results popups

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -5,10 +5,82 @@ import L from "leaflet";
 import React from "react";
 
 import oxfordWards from "./data/oxford-wards.json";
+import results2022 from "./data/results-2022.json";
+
+type WardName = keyof typeof results2022;
+
+type CandidateEntry = {
+  lastName: string
+  names: string
+  description: string
+  party: string
+  votes: number
+  elected: boolean
+
+}
+
+type CandidateProps = {
+  candidate: CandidateEntry
+}
+
+function Candidate(props: CandidateProps) {
+  const { candidate } = props;
+
+  return (<>
+    <div>{candidate.lastName}</div>
+  </>)
+}
+
+type CandidatesProps = {
+  candidates: Array<CandidateEntry>
+}
+
+function Candidates(props: CandidatesProps) {
+  const { candidates } = props;
+
+
+  return (
+    <>
+      {candidates.map((candidate: CandidateEntry) => <Candidate key={candidate.lastName} candidate={candidate} />)}
+    </>
+  )
+}
+
+type WardTooltipProps = {
+  map: L.Map
+  layer: L.Layer
+  wardName: WardName
+}
+
+function WardTooltip(props: WardTooltipProps) {
+  const tooltipRef = React.useRef<HTMLDivElement>(null);
+  const { map, layer, wardName } = props;
+
+  React.useEffect(() => {
+    if (!tooltipRef.current) {
+      return;
+    }
+
+    layer.bindPopup(tooltipRef.current!).openPopup()
+  }, [tooltipRef, map, layer]);
+
+  const data = results2022[wardName];
+
+  return (
+    <>
+      <div ref={tooltipRef}>
+        {wardName}
+        {data && <Candidates candidates={data.candidates} />}
+      </div>
+    </>
+  )
+}
 
 function App() {
   const mapRef = React.useRef<HTMLElement>(null);
-  const [map, setMap] = React.useState<L.Map| null>(null);
+  const [map, setMap] = React.useState<L.Map | null>(null);
+  const [currentWard, setCurrentWard] = React.useState<WardName | null>(null);
+  const [layer, setLayer] = React.useState<L.Layer | null>(null);
 
   // Initialise map
   React.useEffect(() => {
@@ -21,11 +93,12 @@ function App() {
     }
     const mapEl = document.createElement("div")
     mapRef.current.appendChild(mapEl);
+
     setMap(L.map(mapEl).setView([51.755409, -1.255782], 12));
   }, [map, setMap, mapRef]);
 
   React.useEffect(() => {
-    if (map === null ) {
+    if (map === null) {
       return;
     }
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -45,23 +118,27 @@ function App() {
           };
         },
         onEachFeature(feature, layer) {
-          const wardName = feature.properties.Ward_name;
-  
-          layer.bindPopup(wardName);
-          },
+          layer.addEventListener("mouseover", () => {
+            setCurrentWard(feature.properties.Ward_name);
+            setLayer(layer);
+          })
+        },
       }
     ).addTo(map);
-  
-  }, [map])
+
+  }, [map, setCurrentWard, setLayer])
 
   return (
     <>
-    <header className="app">
-      <h1>Oxford City Council</h1>      
-    </header>
-    <main className="app">
-      <section id="map" ref={mapRef}></section>
-    </main>
+      <header className="app">
+        <h1>Oxford City Council</h1>
+      </header>
+      <main className="app">
+        {currentWard && layer && map && (
+          <WardTooltip wardName={currentWard} map={map} layer={layer} />
+        )}
+        <section id="map" ref={mapRef}></section>
+      </main>
     </>
   )
 }
